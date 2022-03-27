9ed90efc0a0c061af2c86d3365fc3392bc5b87a9
Scollar1248 <102401116+Scollar1248@users.noreply.github.com>
2022-03-26 20:56:33 -0400

    Add files via upload

---
+++ P1HW1_ScollardRyan.py
@@ -0,0 +1,8 @@
+firstNumber = int(input("Enter a number:\n"))
+secondNumber = int(input("Enter a number:\n"))
+numSum = firstNumber + secondNumber
+numProduct = firstNumber * secondNumber
+print(firstNumber)
+print(secondNumber)
+print(numSum)
+print(numProduct)

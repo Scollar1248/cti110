369f4c5f0054dc092f5c5aba4bba5104b3794270
Scollar1248 <102401116+Scollar1248@users.noreply.github.com>
2022-04-14 23:07:07 -0400

    Update P2HW1_ScollardRyan.py

--- P2HW1_ScollardRyan.py
+++ P2HW1_ScollardRyan.py
@@ -9,11 +9,11 @@
 sales_tax = .07
 
 #Asks user input for item price stores float in to an item variable.
-item_one = float(input('Enter the price of item # 1: '))
-item_two = float(input('Enter the price of item # 2: '))
-item_three = float(input('Enter the price of item # 3: '))
-item_four = float(input('Enter the price of item # 4: '))
-item_five = float(input('Enter the price of item # 5: '))
+item_one = float(input('Enter the price of item #1: '))
+item_two = float(input('Enter the price of item #2: '))
+item_three = float(input('Enter the price of item #3: '))
+item_four = float(input('Enter the price of item #4: '))
+item_five = float(input('Enter the price of item #5: '))
 
 #Subtotal caculates total price of items without taxes
 subtotal = (item_one+item_two+item_three+item_four+item_five)
@@ -24,14 +24,16 @@ total_taxes = (subtotal*sales_tax)
 #Calcualtes subtotal and sales tax
 total = (subtotal+total_taxes)
 
+#Prints blank line to add seperation between item #5 and results
+print()
 #Outputs results of program calculations 
 print('---------Results----------')
 
 #Outputs subotal of items with a floating point value.
-print(f'Subtotal: ${subtotal:.2f}')
+print(f'Subtotal: {subtotal:.2f}')
 
 #Outputs total Sales Tax of items with a floating point value.
-print(f'Sales Tax: ${total_taxes:.2f}')
+print(f'Sales Tax: {total_taxes:.2f}')
 
 #Outputs Total price of items to include Sales Tax with a floating point value.
-print(f'Total: ${total:.2f}')
+print(f'Total: {total:.2f}')

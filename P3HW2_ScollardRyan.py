37a938b109e1ea9924006a6448506a5321ef23a8
Scollar1248 <102401116+Scollar1248@users.noreply.github.com>
2022-04-29 23:10:08 -0400

    Create P3HW2_ScollardRyan.py
    Pizza Order

---
+++ P3HW2_ScollardRyan.py
@@ -0,0 +1,48 @@
+# Pizza Order 
+# 18 APR 2022
+# CTI-110 P3HW2 - Pizza Order
+# Ryan Scollard
+#
+
+import math
+
+#Ask user for number of students
+numStudents = int(input("How many students do you want to order pizza for? "))
+#Ask user if they are going to share
+number_share = float(input('Enter the number of people per pizza (1.5, 2 or 3): '))
+
+#List of approved people per pizza
+numshareList = [1.5, 2, 3]
+
+#Checks to see if number of people sharing the pizza is in the approved list. If not pop error else pass.
+if number_share not in numshareList:
+    print('\nINVALID ENTRY !!!!!')
+    print('Should have entered 1.5, 2 or 3')
+    print()
+    print('Run Program agan')
+    exit()
+else:
+    pass
+
+#Figures out number of pizzas required by taking number of students divided by people sharing each pizza
+numPizza = numStudents / number_share
+
+#Always got to pay the man
+sales_tax = .06
+
+#Price of pizza
+pizza_price = 5
+
+#Subtotal prices, total taxes, and total price
+subtotal = (numPizza * pizza_price)
+total_taxes = (subtotal*sales_tax)
+total_price = (subtotal + total_taxes)
+
+
+#Prints order
+print("\n-------Pizza Order-------")
+print("Number of Students:", numStudents)
+print("Pizzas Needed:", math.ceil(numPizza)) #Print number of pizza, and round up if there is a float so everyone gets pizza.
+print(f'Price: ${total_price:.2f}')#Fstring to make money look pretty
+print("-------------------------")
+        
